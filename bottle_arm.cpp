80afd93ffe4483bab133d4576e85009f2983f3ab
Bottlehill <214173097@qq.com>
2016-08-26 13:53:57 +0800

    Update bottle_arm.cpp

--- bottle_arm.cpp
+++ bottle_arm.cpp
@@ -1,1 +1,16 @@
+#include "bottle_arm.h"
+#include "math.h"
 
+bottle::bottle1()
+{
+}
+void bottle1::length(int a1,int a2)
+{
+}
+void bottle1::coordinate(double a,double b,double c)
+{
+  double x,y;
+  
+  
+  
+}
