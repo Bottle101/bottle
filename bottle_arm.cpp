4be051170bce94acea44f4733198c1c750b421fd
Bottlehill <214173097@qq.com>
2016-09-04 14:34:24 +0800

    Update bottle_arm.cpp

--- bottle_arm.cpp
+++ bottle_arm.cpp
@@ -11,6 +11,7 @@ void bottle1::coordinate(double x,double y,double z)
 {
   double b;
   b = sqrt(x*x+y*y);
+  double tran[];
   double x1 = (x/z+sqrt(pow(x,2)/pow(z,2)-2*(b*b+c*c-a*a-(2*z*pow(c,2))/z))/2;
   double y1 = sqrt(c*c-x1*x1);
   
